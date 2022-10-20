426dbdbdc02abe31ddcf5ac54d6ab10c64415796
leonardocosta <sir.costa@yahoo.com.br>
2022-10-20 11:07:17 -0300

    Estrutura e estiliza Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
 
 import './global.css';
@@ -5,6 +6,7 @@ import './global.css';
 export function App() {
   return (
     <>
+      <Header />
       <Sidebar />
     </>
   );
