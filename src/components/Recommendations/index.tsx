80f2db4973e3cc60b0db75421d34733d86286b4a
leonardocosta <sir.costa@yahoo.com.br>
2022-10-27 12:47:29 -0300

    Adiciona seção Recomendações no dashboard estrutura e estilo

--- src/components/Recommendations/index.tsx
+++ src/components/Recommendations/index.tsx
@@ -12,19 +12,25 @@ export function Recommendations() {
       address: "Rua das orquídeas, 88, Nova Ipiranga, RJ",
     },
     {
-      id: 1324,
+      id: 2345,
       image: houseImg,
       name: "Casa em Ipiranga",
       address: "Rua das orquídeas, 88, Nova Ipiranga, RJ",
     },
     {
-      id: 1234,
+      id: 3456,
       image: houseImg,
       name: "Casa em Ipiranga",
       address: "Rua das orquídeas, 88, Nova Ipiranga, RJ",
     },
     {
-      id: 1324,
+      id: 4567,
+      image: houseImg,
+      name: "Casa em Ipiranga",
+      address: "Rua das orquídeas, 88, Nova Ipiranga, RJ",
+    },
+    {
+      id: 5678,
       image: houseImg,
       name: "Casa em Ipiranga",
       address: "Rua das orquídeas, 88, Nova Ipiranga, RJ",
